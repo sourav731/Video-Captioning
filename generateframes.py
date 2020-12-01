16f5a1247295892ba1d11e91f13594d310494af7
Sourav Prasad <souravprasad731@gmail.com>
2020-12-01 12:36:38 +0530

    added exception handling

--- generateframes.py
+++ generateframes.py
@@ -10,13 +10,20 @@ class GenerateFrame:
 
     def generate_url(self):
         url = 'https://www.youtube.com/watch?v='+self.video_id
+        return url
 
-    def extract_info(self, url):
+    def extract_details(self):
+        url = self.generate_url()
         ydl_info = {}
         ydl = youtube_dl.YoutubeDL(ydl_info)
-
-        info_dict = ydl.extract_info(url, download=False)
-
+        
+        try:
+            info_dict = ydl.extract_info(url, download=False)
+            
+        except:
+            print('generateframes.py : cannot download')
+            return -1
+        
         return info_dict
 
     def create_temporary_folder(self, path):
@@ -25,16 +32,22 @@ class GenerateFrame:
         except OSError:
             print('generateframes.py : unable to create a directory')
 
-        return
-
-    def generate_frames(self, info_dict, path, folder_name):
+    def generate_frames(self, path, folder_name):
+        info_dict = self.extract_details()
+        
+        if info_dict == -1:
+            return 
+        
         formats = info_dict.get('formats', None)
         temp_folder_path = os.path.join(path, folder_name)
-
-        create_temporary_folder(temp_folder_path)
+        
+        if os.path.isdir(temp_folder_path):
+            self.delete_temporary_files(path,folder_name)
+            
+        self.create_temporary_folder(temp_folder_path)
 
         for f in formats:
-            if f.get('format_note', None) == '144p':
+            if ((f.get('format_note', None) == '144p') or (f.get('format_note', None) == '240p') or (f.get('format_note', None) == '360p')):
                 url = f.get('url', None)
                 cap = cv2.VideoCapture(url)
 
@@ -57,5 +70,8 @@ class GenerateFrame:
         cv2.destroyAllWindows()
 
     def delete_temporary_files(self, path, folder_name):
-        temp_folder_path = os.path.join(path, folder_name)
-        shutil.rmtree(temp_folder_path)
+        try:
+            temp_folder_path = os.path.join(path, folder_name)
+            shutil.rmtree(temp_folder_path)
+        except:
+            print('cannot delete folder')
